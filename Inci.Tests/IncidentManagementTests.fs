e6564aafca8715f09d60bfdb4732d828873c66fd
Luke Hopton <luke@lukehopton.com>
2023-08-15 23:27:45 +0100

    Add event management functions

--- Inci.Tests/IncidentManagementTests.fs
+++ Inci.Tests/IncidentManagementTests.fs
@@ -48,8 +48,101 @@ let ``add event sets sequential ID`` () =
             |> observed (Events.now()) "Obs 1"
             |> observed (Events.now()) "Obs 2"
             |> observed (Events.now()) "Obs 3"
-    let e = List.head i.Events
     Assert.Equal(4, List.length(i.Events))
-    Assert.Equal("o2", e.Id)
-    Assert.Equal("Obs 3", e.Description)
+    let o0 = List.find (fun e -> e.Description = "Obs 1") i.Events
+    Assert.Equal("o0", o0.Id)
+    let o1 = List.find (fun e -> e.Description = "Obs 2") i.Events
+    Assert.Equal("o1", o1.Id)
+    let o2 = List.find (fun e -> e.Description = "Obs 3") i.Events
+    Assert.Equal("o2", o2.Id)
     
+[<Fact>]
+let ``acted adds action`` () =
+    let t = Events.now()
+    let a = "Turned it off and on again"
+    let i = declare "Stuff broke" (Events.now())
+            |> acted t a
+    let action = List.find (fun e -> e.Type = Action) i.Events
+    Assert.Equal(t, action.Time)
+    Assert.Equal(a, action.Description)
+    Assert.False(action.IsResolved)
+
+[<Fact>]
+let ``alertFired adds alert`` () =
+    let t = Events.now()
+    let a = "CPU alert"
+    let i = declare "Stuff broke" (Events.now())
+            |> alertFired t a
+    let alert = List.find (fun e -> e.Type = Alert) i.Events
+    Assert.Equal(t, alert.Time)
+    Assert.Equal(a, alert.Description)
+    Assert.False(alert.IsResolved)
+
+[<Fact>]
+let ``down adds monitor down`` () =
+    let t = Events.now()
+    let m = "Availability"
+    let i = declare "Stuff broke" (Events.now())
+            |> down t m
+    let monitor = List.find (fun e -> e.Type = Monitor) i.Events
+    Assert.Equal(t, monitor.Time)
+    Assert.Equal(m, monitor.Description)
+    Assert.False(monitor.IsResolved)
+
+[<Fact>]
+let ``alertResolved adds resolution event`` () =
+    let a = "Test"
+    let i  = declare "Stuff broke" (Events.now()) |> alertFired (Events.now()) a
+    let id = List.find (fun e -> e.Type = Alert) i.Events
+             |> fun e -> e.Id
+    let t = Events.now()
+    let result = alertResolved t id i
+    Assert.True(result.IsSome)
+    let i2 = result.Value
+    let resolved = List.find (fun e -> e.Type = Alert && e.IsResolved) i2.Events
+    Assert.Equal(t, resolved.Time)
+    Assert.Equal(a, resolved.Description)
+
+[<Fact>]
+let ``alertResolved handles missing ID`` () =
+    let i  = declare "Stuff broke" (Events.now())
+    let t = Events.now()
+    let result = alertResolved t "a99" i
+    Assert.True(result.IsNone)
+
+[<Fact>]
+let ``alertResolved ignores duplicate resolution`` () =
+    let i  = declare "Stuff broke" (Events.now()) |> alertFired (Events.now()) "Test"
+    let id = List.find (fun e -> e.Type = Alert) i.Events
+             |> fun e -> e.Id
+    let t1 = Events.now()
+    let i2 = alertResolved t1 id i
+    let t2 = Events.now()
+    let result = alertResolved t2 id i2.Value
+    Assert.True(result.IsSome)
+    let i3 = result.Value
+    let resolutions = List.filter (fun e -> e.Type = Alert && e.IsResolved) i3.Events
+    let resolution = Assert.Single(resolutions)
+    Assert.Equal(t1, resolution.Time)
+
+[<Fact>]
+let ``up adds resolution event`` () =
+    let m = "Test"
+    let i  = declare "Stuff broke" (Events.now()) |> down (Events.now()) m
+    let id = List.find (fun e -> e.Type = Monitor) i.Events
+             |> fun e -> e.Id
+    let t = Events.now()
+    let result = up t id i
+    Assert.True(result.IsSome)
+    let i2 = result.Value
+    let resolved = List.find (fun e -> e.Type = Monitor && e.IsResolved) i2.Events
+    Assert.Equal(t, resolved.Time)
+    Assert.Equal(m, resolved.Description)
+
+[<Fact>]
+let ``up handles invalid type id`` () =
+    let i = declare "Stuff broke" (Events.now())
+            |> alertFired (Events.now()) "Alert"
+    let alert = List.find (fun e -> e.Type = Alert) i.Events
+    let i2 = up (Events.now()) alert.Id i
+    Assert.True(i2.IsNone)
--- Inci/Core/IncidentManagement.fs
+++ Inci/Core/IncidentManagement.fs
@@ -1,27 +1,5 @@
 module Inci.Core.IncidentManagement
 
-open System
-
-let private addEvent e i =
-    { i with Events = e :: i.Events }
-
-let declare name start =
-    { Id = Guid.NewGuid()
-      Name = name 
-      Events = [{ Id = "d0"
-                  Type = Declaration
-                  Time = start
-                  Description = "Incident declared"
-                  IsResolved = false
-               }]
-      }
-
-let resolve time i =
-    addEvent { Id = "d1"; Type = Declaration; Time = time; Description = "Incident resolved"; IsResolved = true} i
-
-let rename name i =
-    { i with Name = name }
-
 let private idPrefix t =
     match t with
     | Declaration -> "d"
@@ -42,6 +20,54 @@ let private nextId t i =
     | [_] -> prefix + "1"
     | _ -> List.sortByDescending (fun e -> e.Time.Timestamp) events |> List.head |> idAfter prefix
 
+let private addEvent y t s r i =
+    let id = nextId y i
+    let e = { Id = id; Type = y; Time = t; Description = s; IsResolved = r}
+    { i with Events = e :: i.Events }
+
+let private resolutionOf time (event : Event) =
+    { Id = event.Id; Type = event.Type; Time = time; Description = event.Description; IsResolved = true }
+
+let private addResolution eventType id t i =
+    let alert = List.tryFind (fun e -> e.Type = eventType && e.Id = id && not e.IsResolved) i.Events
+    let resolution = List.tryFind (fun e -> e.Type = eventType && e.Id = id && e.IsResolved) i.Events
+    match (alert, resolution) with
+    | (Some _, Some _) -> Some i
+    | (Some event, None) -> Some { i with Events = resolutionOf t event :: i.Events }
+    | (None, _) -> None
+                        
+
+let declare name start =
+    { Id = System.Guid.NewGuid()
+      Name = name 
+      Events = [{ Id = "d0"
+                  Type = Declaration
+                  Time = start
+                  Description = "Incident declared"
+                  IsResolved = false
+               }]
+      }
+
+let resolve time i =
+    addEvent Declaration time "Incident resolved" true i
+
+let rename name i =
+    { i with Name = name }
+
 let observed time s i =
-    let id = nextId Observation i
-    addEvent { Id = id; Type = Observation; Time = time; Description = s; IsResolved = false} i
+    addEvent Observation time s false i
+
+let acted time s i =
+    addEvent Action time s false i
+
+let alertFired time s i =
+    addEvent Alert time s false i
+
+let alertResolved time id i =
+    addResolution Alert id time i
+
+let down time s i =
+    addEvent Monitor time s false i
+
+let up time id i =
+    addResolution Monitor id time i
