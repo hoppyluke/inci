5668a4b3b684af28f65caaabaf4f4c3ab05466ca
Luke Hopton <luke@lukehopton.com>
2023-11-23 23:41:53 +0000

    define event sort order

--- Inci.Tests/EventTimeTests.fs
+++ Inci.Tests/EventTimeTests.fs
@@ -22,3 +22,25 @@ let ``compare sorts by approx. first`` () =
     let e1 = EventTime(t, true)
     let e2 = EventTime(t, false)
     Assert.True(e1 > e2)
+
+[<Fact>]
+let ``comparing events sorts by time`` () =
+    let t1 = EventTime(DateTimeOffset.UtcNow, false)
+    let t2 = EventTime(DateTimeOffset.UtcNow.AddMinutes(10), false)
+    let e1 = { Id="2"; Type=Observation; Time=t1; Description="Test"; IsResolved=false}
+    let e2 = { Id="1"; Type=Observation; Time=t2; Description="Test"; IsResolved=false}
+    Assert.True(e1 < e2)
+
+[<Fact>]
+let ``comparing events sorts by time then ID`` () =
+    let t = EventTime(DateTimeOffset.UtcNow, false)
+    let e1 = { Id="a"; Type=Observation; Time=t; Description="Test"; IsResolved=false}
+    let e2 = { Id="b"; Type=Observation; Time=t; Description="Test"; IsResolved=false}
+    Assert.True(e1 < e2)
+
+[<Fact>]
+let ``comparing events sorts by time then ID then IsResolved`` () =
+    let t = EventTime(DateTimeOffset.UtcNow, false)
+    let e1 = { Id="a1"; Type=Alert; Time=t; Description="Test"; IsResolved=false}
+    let e2 = { Id="a1"; Type=Alert; Time=t; Description="Test"; IsResolved=true}
+    Assert.True(e1 < e2)
--- Inci/Core/Types.fs
+++ Inci/Core/Types.fs
@@ -2,6 +2,12 @@ namespace Inci.Core
 
 open System
 
+module Internal =
+    let thenBy x y comparison =
+        match comparison with
+        | 0 -> compare x y
+        | n -> n
+
 type EventTime =
     struct
         val Timestamp: DateTimeOffset
@@ -18,12 +24,33 @@ type EventType =
     | Alert
     | Declaration
 
+[<CustomComparison; CustomEquality>]
 type Event =
     { Id: string
       Type: EventType
       Time: EventTime
       Description: string
       IsResolved: bool }
+    
+    interface IComparable<Event> with
+        member this.CompareTo other =
+            compare this.Time other.Time
+            |> Internal.thenBy this.Id other.Id
+            |> Internal.thenBy this.IsResolved other.IsResolved
+    
+    interface IComparable with
+        member this.CompareTo obj =
+            match obj with
+            | :? Event as other -> (this :> IComparable<Event>).CompareTo other
+            | _ -> invalidArg (nameof obj) "Not an Event"
+
+    override this.Equals obj =
+        match obj with
+        | :? Event as other -> this.Id = other.Id && this.IsResolved = other.IsResolved
+        | _ -> false
+
+    override this.GetHashCode () =
+        this.Id.GetHashCode() ||| this.IsResolved.GetHashCode()
 
 type Incident =
     { Id: Guid
